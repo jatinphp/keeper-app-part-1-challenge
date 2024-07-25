4101ac207ed87f9732e4d01fbc467ab88219fd07
Jatin <32764697+jatinphp@users.noreply.github.com>
2024-07-25 09:48:31 +0000

    keeper code

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Note Title</h1>
+      <p>This is a content area </p>
+    </div>
+  );
+}
+
+export default Note;
--- src/index.jsx
+++ src/index.jsx
@@ -16,3 +16,8 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
