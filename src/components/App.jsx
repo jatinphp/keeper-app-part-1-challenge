e47482514a2bcfa45dbe93c213d60f576fc567df
Jatin <32764697+jatinphp@users.noreply.github.com>
2024-07-25 11:18:16 +0000

    props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <div>
       <Header />
-      <Note />
+      <Note title="Title Props 1" content="This is first block content" />
+      <Note title="Title Props 2" content="This is second block content" />
+      <Note title="Title Props 3" content="This is third block content" />
       <Footer />
     </div>
   );
